26f86a7474a65a094fd607268feefa3106126b82
bnew27101-hue <bnew27101@gmail.com>
2025-12-19 00:55:12 -1200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -667,6 +667,6 @@ def handle_stop_session():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=False,debug=False, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=port, debug=False,allow_unsafe_werkzeug=True)
 
 
